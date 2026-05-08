6275df6a694e40b87cfdcb9294d1098f626436ff
Radai <lordradai@null.net>
2026-05-08 14:18:06 +0200

    fix

--- GuiFramework/GS/GuiFramework/System/GUIVirtualInput.h
+++ GuiFramework/GS/GuiFramework/System/GUIVirtualInput.h
@@ -51,16 +51,6 @@ namespace GuiFramework
         {
             return InputID == eventID;
         }
-
-        static void* operator new(dl_size size)
-        {
-            return DLKR::AllocateAligned(size, 4, GUI_ALLOCATOR);
-        }
-
-        static void operator delete(void* p)
-        {
-            DLKR::Free(p, GUI_ALLOCATOR);
-        }
     };
 
     typedef _GUI_VIRTUAL_INPUT VirtualInput;
