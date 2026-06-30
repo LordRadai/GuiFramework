2c8a8b0f313090e18f49fef5a2745e2361023339
Radai <lordradai@null.net>
2026-06-30 16:38:46 +0200

    Update GUITweakerDialog.h

--- GuiFramework/GS/GuiFramework/Window/Tweaker/GUITweakerDialog.h
+++ GuiFramework/GS/GuiFramework/Window/Tweaker/GUITweakerDialog.h
@@ -27,9 +27,9 @@ namespace GuiFramework
 		GUITweakerGroup* BeginGroup(TGUISharedString<dl_wchar> label, dl_uint flags = 0);
 		void EndGroup();
 
-		TGUIColorTweaker<DLMT::DL_COLOR_32>* CreateColorTweaker(TGUISharedString<dl_wchar> label, DLMT::DL_COLOR_32* v, dl_uint flags);
-		TGUIColorTweaker<DLMT2::DL_COLOR_U8>* CreateColorTweaker(TGUISharedString<dl_wchar> label, DLMT2::DL_COLOR_U8* v, dl_uint flags);
-		TGUIColorTweaker<DLMT::DL_VECTOR4>* CreateColorTweaker(TGUISharedString<dl_wchar> label, DLMT::DL_VECTOR4* v, dl_uint flags);
+		TGUIColorTweaker<DLMT::DL_COLOR_32>* CreateColorTweaker(TGUISharedString<dl_wchar> label, DLMT::DL_COLOR_32* v, dl_uint flags = 0);
+		TGUIColorTweaker<DLMT2::DL_COLOR_U8>* CreateColorTweaker(TGUISharedString<dl_wchar> label, DLMT2::DL_COLOR_U8* v, dl_uint flags = 0);
+		TGUIColorTweaker<DLMT::DL_VECTOR4>* CreateColorTweaker(TGUISharedString<dl_wchar> label, DLMT::DL_VECTOR4* v, dl_uint flags = 0);
 
 		GUITextureList* CreateTextureList(TGUISharedString<dl_wchar> label);
 		GUITextureViewer* CreateTextureViewer(TGUISharedString<dl_wchar> label, GUITextureBase* pTexture);
