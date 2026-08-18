ae4afb3fc92d82f6ce7cecb5030fc4f3e2a3e7d6
Radai <lordradai@null.net>
2026-08-18 08:40:30 +0200

    Update Globals.h

--- GuiFramework/GS/GuiFramework/Globals.h
+++ GuiFramework/GS/GuiFramework/Globals.h
@@ -6,8 +6,8 @@ namespace GuiFramework
 {
 	namespace GUIAllocator
 	{
-#define GUI_ALLOCATOR *(DLKR::DLAllocator**)(MODULE_ADDR + 0x1617108)
-#define STRING_ALLOCATOR *(DLKR::DLAllocator**)(MODULE_ADDR + 0x1617110)
-#define GUI_STL_ALLOCATOR *(DLKR::DLAllocator**)(MODULE_ADDR + 0x1617118)
+#define GUI_ALLOCATOR       (*(DLKR::DLAllocator**)(MODULE_ADDR + 0x1617108))
+#define STRING_ALLOCATOR    (*(DLKR::DLAllocator**)(MODULE_ADDR + 0x1617110))
+#define GUI_STL_ALLOCATOR   (*(DLKR::DLAllocator**)(MODULE_ADDR + 0x1617118))
 	}
 }
